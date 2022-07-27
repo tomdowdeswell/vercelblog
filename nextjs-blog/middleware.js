6eae78d8ccf92555cfe6a14df11d1c9b7e9addaf
Dowdtw1 <103059208+Dowdtw1@users.noreply.github.com>
2022-07-27 17:19:47 +0100

    Create middleware.js

---
+++ nextjs-blog/middleware.js
@@ -0,0 +1,8 @@
+// middleware.ts
+export default function middleware(request) {
+  return Response.redirect(new URL('/about-2', request.url));
+}
+// config with custom matcher
+export const config = {
+  matcher: '/posts/:path*',
+};
