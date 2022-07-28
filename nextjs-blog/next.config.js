4247dc81636388de66f2a34a3963550920b7c7a8
tomdowdeswell <110198855+tomdowdeswell@users.noreply.github.com>
2022-07-28 21:16:00 +0100

    Update next.config.js

--- nextjs-blog/next.config.js
+++ nextjs-blog/next.config.js
@@ -2,12 +2,20 @@ module.exports = () => {
   const rewrites = () => {
     return [
       {
-        source: "/zephr/*",
-        destination: "https://tomd-vercel_personal_test.cdn.zephr.com/zephr/*",
+        source: "/zephr/feature",
+        destination: "https://tomd-vercel_personal_test.cdn.zephr.com/zephr/feature",
       },
       {
-        source: "/blaize/*",
-        destination: "https://tomd-vercel_personal_test.cdn.zephr.com/blaize/*",
+        source: "/blaize/feature",
+        destination: "https://tomd-vercel_personal_test.cdn.zephr.com/blaize/feature",
+      },
+      {
+        source: "/zephr/feature-decisions",
+        destination: "https://tomd-vercel_personal_test.cdn.zephr.com/zephr/feature-decisions",
+      },
+      {
+        source: "/blaize/feature-decisions",
+        destination: "https://tomd-vercel_personal_test.cdn.zephr.com/blaize/feature-decisions",
       },
     ];
   };
