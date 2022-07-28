dbbd56336f7ec782666243cc346ad8eef376e4df
Dowdtw1 <103059208+Dowdtw1@users.noreply.github.com>
2022-07-28 09:34:01 +0100

    Update _document.js

--- nextjs-blog/pages/_document.js
+++ nextjs-blog/pages/_document.js
@@ -1,4 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script'
+
+
+
 export default class MyDocument extends Document {
   render() {
     return (
