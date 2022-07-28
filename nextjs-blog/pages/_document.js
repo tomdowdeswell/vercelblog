a559c69a5111a1ac2aa08e78e8de8107fd0c2543
tomdowdeswell <110198855+tomdowdeswell@users.noreply.github.com>
2022-07-28 21:31:34 +0100

    Update _document.js

--- nextjs-blog/pages/_document.js
+++ nextjs-blog/pages/_document.js
@@ -1,4 +1,4 @@
-const zephr = "https://assets.zephr.com/zephr-browser/1.3.11/zephr-browser.umd.js"
+
 
 
 import { Html, Head, Main, NextScript } from 'next/document'
@@ -8,7 +8,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-//         <script src="https://assets.zephr.com/zephr-browser/1.3.11/zephr-browser.umd.js"></script>
       </Head>
       <body>
         <Main />
