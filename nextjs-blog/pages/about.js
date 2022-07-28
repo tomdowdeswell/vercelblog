5e361234a6defb7a9704a519d38a966454a76e08
Dowdtw1 <103059208+Dowdtw1@users.noreply.github.com>
2022-07-28 19:24:42 +0100

    Update about.js

--- nextjs-blog/pages/about.js
+++ nextjs-blog/pages/about.js
@@ -1,5 +1,25 @@
 function About() {
-  return <div>About</div>
+  return <p class = "article"> `wall of text
+          Jump to navigationJump to search
+
+          Contents
+          1	English
+          1.1	Noun
+          1.1.1	Alternative forms
+          1.1.2	Translations
+          1.1.3	See also
+          English
+          Noun
+          wall of text (plural walls of text)
+
+          (chiefly Internet slang) An intimidatingly large block of writing, particularly one with few or no paragraph breaks.
+          Alternative forms
+          text wall
+          Translations
+          ±an intimidatingly large block of writing
+          See also
+          copypasta
+          info-dump` </p>
 }
 
 export default About
