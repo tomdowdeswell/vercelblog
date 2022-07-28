439f158a07f644522253acaddc9764c7013755e7
Dowdtw1 <103059208+Dowdtw1@users.noreply.github.com>
2022-07-28 20:34:00 +0100

    Update _app.js

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import React, { useState, useEffect } from 'react'
 import Script from 'next/script'
 import Head from 'next/head'
-const zephr = "https://assets.zephr.com/zephr-browser/1.3.11/zephr-browser.umd.js"
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => { 
