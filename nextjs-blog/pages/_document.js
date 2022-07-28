05a98d29e436f09485efd52bb0c85053c31ec36e
Dowdtw1 <103059208+Dowdtw1@users.noreply.github.com>
2022-07-28 19:58:21 +0100

    Update _document.js

--- nextjs-blog/pages/_document.js
+++ nextjs-blog/pages/_document.js
@@ -1,18 +1,18 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+const zephr = "https://assets.zephr.com/zephr-browser/1.3.11/zephr-browser.umd.js"
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head> 
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script src={zephr}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
 
 
