bc96b06d156618d769381d58f135569d9ec0e4e8
Dowdtw1 <103059208+Dowdtw1@users.noreply.github.com>
2022-07-28 20:25:49 +0100

    Create next.config.js

---
+++ nextjs-blog/next.config.js
@@ -0,0 +1,13 @@
+module.exports = () => {
+  const rewrites = () => {
+    return [
+      {
+        source: "/zephr/features",
+        destination: "httsp://tomd-vercel.cdn.zephr.com/zephr/features",
+      },
+    ];
+  };
+  return {
+    rewrites,
+  };
+};
