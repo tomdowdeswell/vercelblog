d3c71091e4a2b2a1ce955b3a8c7e666c9ce8670d
Dowdtw1 <103059208+Dowdtw1@users.noreply.github.com>
2022-07-28 11:08:34 +0100

    Update _app.js

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -26,6 +26,53 @@ function MyApp() {
           copypasta
           info-dump 
         </p>
+  
+    <p class = "article-2"> wall of text
+          Jump to navigationJump to search
+
+          Contents
+          1	English
+          1.1	Noun
+          1.1.1	Alternative forms
+          1.1.2	Translations
+          1.1.3	See also
+          English
+          Noun
+          wall of text (plural walls of text)
+
+          (chiefly Internet slang) An intimidatingly large block of writing, particularly one with few or no paragraph breaks.
+          Alternative forms
+          text wall
+          Translations
+          ±an intimidatingly large block of writing
+          See also
+          copypasta
+          info-dump 
+        </p>
+  
+  
+  <p class = "article-3"> wall of text
+          Jump to navigationJump to search
+
+          Contents
+          1	English
+          1.1	Noun
+          1.1.1	Alternative forms
+          1.1.2	Translations
+          1.1.3	See also
+          English
+          Noun
+          wall of text (plural walls of text)
+
+          (chiefly Internet slang) An intimidatingly large block of writing, particularly one with few or no paragraph breaks.
+          Alternative forms
+          text wall
+          Translations
+          ±an intimidatingly large block of writing
+          See also
+          copypasta
+          info-dump 
+        </p>
    </>
   )
 }
