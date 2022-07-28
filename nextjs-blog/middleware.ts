dcc638601d80379616956c759d578d0ef2374e56
Dowdtw1 <103059208+Dowdtw1@users.noreply.github.com>
2022-07-28 09:14:31 +0100

    Delete middleware.ts

--- nextjs-blog/middleware.ts
+++
@@ -1,8 +0,0 @@
-// middleware.ts
-// export default function middleware(request) {
-//   return Response.redirect(new URL('/about-2', request.url));
-// }
-// // config with custom matcher
-// export const config = {
-//   matcher: '/:path*',
-// };
