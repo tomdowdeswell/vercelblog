f5e1e0a9d276677fb513d4f118bc19111419182d
tomdowdeswell <110198855+tomdowdeswell@users.noreply.github.com>
2022-07-28 21:28:41 +0100

    Update _document.js

--- nextjs-blog/pages/_document.js
+++ nextjs-blog/pages/_document.js
@@ -2,13 +2,13 @@ const zephr = "https://assets.zephr.com/zephr-browser/1.3.11/zephr-browser.umd.j
 
 
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+// import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <script src="https://assets.zephr.com/zephr-browser/1.3.11/zephr-browser.umd.js"></script>
+//         <script src="https://assets.zephr.com/zephr-browser/1.3.11/zephr-browser.umd.js"></script>
       </Head>
       <body>
         <Main />
@@ -18,23 +18,3 @@ export default function Document() {
   )
 }
 
-
-// import Script from 'next/script'
-// const zephr = "https://assets.zephr.com/zephr-browser/1.3.11/zephr-browser.umd.js"
-// import Document, { Html, Head, Main, NextScript } from 'next/document'
-
-// export default class MyDocument extends Document {
-//   render() {
-//     return (
-//       <Html>
-//         <Head>
-//           <script src={zephr} />
-//         </Head>
-//         <body>
-//           <Main />
-//           <NextScript />
-//         </body>
-//       </Html>
-//     )
-//   }
-// }
