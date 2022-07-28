0dd8ed189ad23e28fc77a19edf80758ac143c064
Dowdtw1 <103059208+Dowdtw1@users.noreply.github.com>
2022-07-28 09:35:40 +0100

    Update first-post.js

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -9,6 +9,28 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
+      <p class = "article"> wall of text
+          Jump to navigationJump to search
+
+          Contents
+          1	English
+          1.1	Noun
+          1.1.1	Alternative forms
+          1.1.2	Translations
+          1.1.3	See also
+          English
+          Noun
+          wall of text (plural walls of text)
+
+          (chiefly Internet slang) An intimidatingly large block of writing, particularly one with few or no paragraph breaks.
+          Alternative forms
+          text wall
+          Translations
+          ±an intimidatingly large block of writing
+          See also
+          copypasta
+          info-dump 
+        </p>
     </>
   );
 }
