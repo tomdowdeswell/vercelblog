70a2557a57eda95aea8d712677a06cf49eff30ef
Dowdtw1 <103059208+Dowdtw1@users.noreply.github.com>
2022-07-28 13:16:54 +0100

    Update _app.js

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -15,12 +15,7 @@ function MyApp() {
     <>
     <script src = { zephr } />
     
-    <button onClick={() => setResourceType('refresh')}>Refresh</button>
-    <button onClick={() => setResourceType('article')}>Article</button>
-
-    <h1>{resourceType}</h1>
-
-    <p class = "article"> wall of text
+    <button onClick={() => setResourceType('wall of text
           Jump to navigationJump to search
 
           Contents
@@ -40,7 +35,31 @@ function MyApp() {
           ±an intimidatingly large block of writing
           See also
           copypasta
-          info-dump 
+          info-dump ')}>Refresh</button>
+    <button onClick={() => setResourceType('wall of text
+          Jump to navigationJump to search
+
+          Contents
+          1	English
+          1.1	Noun
+          1.1.1	Alternative forms
+          1.1.2	Translations
+          1.1.3	See also
+          English
+          Noun
+          wall of text (plural walls of text)
+
+          (chiefly Internet slang) An intimidatingly large block of writing, particularly one with few or no paragraph breaks.
+          Alternative forms
+          text wall
+          Translations
+          ±an intimidatingly large block of writing
+          See also
+          copypasta
+          info-dump ')}>Article</button>
+
+
+    <p class = "article"> {resourceType} 
         </p>
   
     <p class = "article-2"> wall of text
